7522cba7155f64906a1d0947115678c4ccc5501f
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-03-14 14:00:37 +0330

    Update 0_intro_python_programming.py

--- 0_intro_python_programming.py
+++ 0_intro_python_programming.py
@@ -1,1 +1,63 @@
 #---------NOTHIGN YET
+
+
+
+#ipython console and Shel commands
+
+'''
+echo 'hello world''       #echo is lik epython's print function
+pwd                     #pwd= print working directory
+ls                     #list working diectory contents
+
+cd project                 #cd -->change directory 
+
+mkdir myproject ----->   #mkdir= make new directory
+cd myproject 
+
+mv ../myproject.text ./  #move from one directory up(../) to the current directory (./)
+
+
+cd ~
+cd ..
+cd -
+
+
+
+
+Ipython commands
+you can use ls, mkdir, cd ,....
+you can run your code
+
+%xmode verbose ---> The exception reporing mdoe : Verbose --> adds some extra information
+
+%debug --> it can get us the debugging ability 
+
+
+Parital list of debugging commands
+
+list     show the current location in the file
+h(elp)  show list of command
+q(uit)  quit the debugger and the program
+c(ontinue)   quit the debugger, contineu in program
+n(ext)    go to the next setop of the program
+<enter>   repoear the previous command
+
+p(rint)  print vairbales
+s(tep)   step into subroutine
+r(eturn)   retuen out of subroutine
+
+
+
+%time    time th eexcdetution of single statement
+
+%timeit    TIME REPEATED EXCETUTION SINGL STATEMENT FOR MROE ACCURACY
+%prun     Run code with profiler
+%lprun       line-by-line profiler
+
+%memit     measure the memory use of single statement
+
+%mprun    run code with line by ine memory profiler
+
+
+
+'''
