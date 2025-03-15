952417149aa5877c487bfc9a1adcd73fc17ed163
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-03-16 01:31:25 +0330

    Update 4_Numerical_Modeling.py

--- 4_Numerical_Modeling.py
+++ 4_Numerical_Modeling.py
@@ -1,2 +1,72 @@
+'''
 
-##
+In The Name Of GOD
+
+Ali Pilehvar Meibody
+
+
+Table of Contents:
+
+Numerical Modeling
+
+
+
+'''
+
+
+
+'''
+---------------------------------------
+------- Numerical Moddeling -----------
+---------------------------------------
+
+
+We have these types of equations that must be solved:
+In all point of science from heat transfer, mass transfer, fluid mechanic, 
+rheology , molecular dynamic, quantum physic and … we have a lot of equations 
+that can have solution that is root or maybe it is some equations that show 
+the current state of system and needs to be solved. What is these equations? 
+It can be something very lienar that onl;y when yousolve you get the exact 
+like we have 20 distance and we are in x=0 and what is new x . some times 
+is more complicated or non lienar or differential It means that it depends
+on something else and changed based on that .
+
+For solving the equation or equations in any forms we have these two important methods:
+
+
+Analytical Methods provide exact solutions 
+Numerical Methods, on the other hand, offer approximate solutions, which are 
+especially useful when analytical solutions are difficult or impossible to obtain.
+
+
+Process some operations is done on materials. It can be simple transferring from one
+place to other or complicated chemical reaction.
+
+Mathematical modeling achieving equations that represent that process as some condition and input values
+
+
+** equations can be algebraic equation, differential and mix of them
+
+
+
+Modeling approaches:
+There is 4 approaches from theory (base), empirical (experimental), semi empirical , likelihood 
+
+1-Theory (base)-> here we use one or multiple Conservation laws.  
+it can be conservation laws of mass, energy, momentum or population.
+Conservation laws format:
+
+Aggregation = input – output + production – consumption
+
+Then we must select variables.
+
+Dependent variable and independent variable.
+Actually in most cases we have dependent variable (Y) that depend on one independent variable (X) and we can change the independent variable and we want to calculate or se the dependent variable (Y).
+
+We write the conservation laws for element region.
+Element is the biggest region that dependent variable (y) is consistent. This element can be system or control volume. 
+
+
+
+
+''''
