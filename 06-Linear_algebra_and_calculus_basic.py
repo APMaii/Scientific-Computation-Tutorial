e2c7d04214b068e83d1a18eaba4490f64f3637da
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-12-18 21:17:25 +0100

    Update 06-Linear_algebra_and_calculus_basic.py

--- 06-Linear_algebra_and_calculus_basic.py
+++ 06-Linear_algebra_and_calculus_basic.py
@@ -6,8 +6,6 @@ https://github.com/ageron/handson-ml2/blob/master/math_linear_algebra.ipynb
 https://github.com/ageron/handson-ml2/blob/master/math_differential_calculus.ipynb
 
 '''
-
-
 #================================
 #================================
 #================================
