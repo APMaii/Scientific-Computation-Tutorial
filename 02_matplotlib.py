34a9faa166440548e24c3a314a8215f81baf3628
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2026-01-17 19:06:55 +0100

    Update 02_matplotlib.py

--- 02_matplotlib.py
+++ 02_matplotlib.py
@@ -1,6 +1,5 @@
 #from teh book and my tutorials
-
-https://github.com/ageron/handson-ml2/blob/master/tools_matplotlib.ipynb
+#https://github.com/ageron/handson-ml2/blob/master/tools_matplotlib.ipynb
 
 """
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
